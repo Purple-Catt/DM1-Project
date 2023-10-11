719d9c0ad901d7d564c6ebc5ad52187c744a1284
Purple-Catt <marcodelpi@hotmail.com>
2023-10-11 13:47:34 +0200

    Updating files

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import dataframe_image as dfi
 
+# Import train dataset
 dataset = pd.read_csv("train.csv", index_col=0)
+corr = dataset.corr(numeric_only=True)
+# Styling and saving the correlation matrix
+styled = corr.style.background_gradient(cmap="coolwarm", vmin=-1, vmax=1)
+dfi.export(styled, "test.png")
+
